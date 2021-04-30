15a893f316659e919dd421365197f664805f3101
Anwesh43 <anweshthecool0@gmail.com>
2021-05-01 02:17:16 +0530

    adding LBSBNode class

--- linebarslideballview/src/main/java/com/example/linebarslideballview/LineBarSlideBallView.kt
+++ linebarslideballview/src/main/java/com/example/linebarslideballview/LineBarSlideBallView.kt
@@ -126,4 +126,45 @@ class LineBarSlideBallView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBSBNode(var i : Int, val state : State = State()) {
+
+        private var next : LBSBNode? = null
+        private var prev : LBSBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBSBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBSBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBSBNode {
+            var curr : LBSBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
