6235032f960c640e11592ac145f37c3041d47281
Anwesh43 <anweshthecool0@gmail.com>
2021-05-01 01:54:07 +0530

    adding LineBarSlideBallView class

--- linebarslideballview/src/main/java/com/example/linebarslideballview/LineBarSlideBallView.kt
+++ linebarslideballview/src/main/java/com/example/linebarslideballview/LineBarSlideBallView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawLBSBNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineBarSlideBall(scale, w, h, paint)
 }
+
+class LineBarSlideBallView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
