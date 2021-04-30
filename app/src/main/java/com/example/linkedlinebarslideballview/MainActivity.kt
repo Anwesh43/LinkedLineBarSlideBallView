247beef303ff8e17e93e7c81c7ae90aa7e3317d2
Anwesh43 <anweshthecool0@gmail.com>
2021-05-01 02:28:47 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/example/linkedlinebarslideballview/MainActivity.kt
+++ app/src/main/java/com/example/linkedlinebarslideballview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedlinebarslideballview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.linebarslideballview.LineBarSlideBallView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LineBarSlideBallView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
