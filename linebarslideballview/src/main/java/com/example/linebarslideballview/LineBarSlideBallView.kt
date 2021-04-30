0c69eb7f055b52e78a9d9ccb46a2c4a0a8ff9fcc
Anwesh43 <anweshthecool0@gmail.com>
2021-05-01 01:53:02 +0530

    adding Canvas extension methods

--- linebarslideballview/src/main/java/com/example/linebarslideballview/LineBarSlideBallView.kt
+++ linebarslideballview/src/main/java/com/example/linebarslideballview/LineBarSlideBallView.kt
@@ -30,3 +30,36 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawLineBarSlideBall(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val r : Float = Math.min(w, h) / rFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float  = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val sf4 : Float = sf.divideScale(3, parts)
+    save()
+    translate(w / 2, h)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        drawLine(-r, 0f, -r, -size * sf1, paint)
+        drawRect(RectF(-r - (w - r) * sf2, -size, -r, 0f), paint)
+        restore()
+    }
+    save()
+    translate(0f, -h + (h - size -  2* r) * sf4)
+    drawArc(RectF(-r, 0f, r, 2 * r), 0f, 360f * sf3, true, paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawLBSBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBarSlideBall(scale, w, h, paint)
+}
